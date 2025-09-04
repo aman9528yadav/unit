8b1577ccbc114d20b75bac59ca839cd3398881ad
Aman Yadav <amanyadavyadav9458@gmail.com>
2025-09-04 09:09:10 +0000

    ggogle button not visible

--- src/components/welcome-form.tsx
+++ src/components/welcome-form.tsx
@@ -13,6 +13,22 @@ import { auth } from "@/lib/firebase";
 import { signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { Eye, EyeOff } from "lucide-react";
 
+const GoogleIcon = () => (
+    <svg viewBox="0 0 48 48" className="w-5 h-5">
+      <title>Google Logo</title>
+      <clipPath id="g">
+        <path d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z"/>
+      </clipPath>
+      <g clipPath="url(#g)">
+        <path fill="#FBBC05" d="M0 37V11l17 13z"/>
+        <path fill="#EA4335" d="M0 11l17 13 7-6.1L48 14V0H0z"/>
+        <path fill="#34A853" d="M0 37l30-23 7.9 1L48 0v48H0z"/>
+        <path fill="#4285F4" d="M48 48L17 24l-4-3 35-10z"/>
+      </g>
+    </svg>
+);
+
+
 export function WelcomeForm() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -151,7 +167,7 @@ export function WelcomeForm() {
         </div>
         <div className="flex justify-center gap-4">
             <Button onClick={handleGoogleLogin} variant="outline" size="icon" className="rounded-full" disabled={isSubmitting}>
-                <Image src="/google-logo.svg" alt="Google" width={20} height={20} />
+                <GoogleIcon />
             </Button>
         </div>
 
