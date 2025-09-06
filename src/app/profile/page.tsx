755699fe6dad193b2ba23d6210e6f151ceac8dcb
Aman Yadav <amanyadavyadav9458@gmail.com>
2025-09-06 03:25:21 +0000

    profile page make as user data page when i click on profile it redirect

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,10 +1,10 @@
 
-import { Profile } from "@/components/profile";
+import { UserData } from "@/components/userdata";
 
 export default function ProfilePage() {
   return (
-    <main className="flex min-h-screen w-full flex-col items-center bg-background">
-      <Profile />
+    <main className="flex min-h-screen w-full flex-col items-center justify-center bg-background p-4">
+      <UserData />
     </main>
   );
 }
