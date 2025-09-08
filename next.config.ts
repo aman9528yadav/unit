e441b9b4447a788628c5b42cd323d4c7124be4b4
Aman Yadav <amanyadavyadav9458@gmail.com>
2025-09-08 15:30:55 +0000

    ai translater not work in all device why

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 
 import type {NextConfig} from 'next';
 
+const isProd = process.env.NODE_ENV === 'production'
+
 const withPWA = require('@ducanh2912/next-pwa').default({
   dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
+  disable: !isProd,
   register: true,
   skipWaiting: true,
   runtimeCaching: [
